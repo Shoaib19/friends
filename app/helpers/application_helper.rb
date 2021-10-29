90a373ce0a0ab76f48d4f5470229f6852337e221
shoaib019 <shoaibsabir019@gmail.com>
2021-10-29 11:26:16 +0500

    code before js

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def find_name
+        user = User.joins("INNER JOIN friends ON users.id = friends.user_id")
+        user.each do |usr|
+            if usr.id == current_user.id
+                return usr.name
+            end
+        end
+   end
 end
---
+++ db/migrate/20211028100647_add_user_id_to_friends.rb
@@ -0,0 +1,4 @@
+class AddUserIdToFriends < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
