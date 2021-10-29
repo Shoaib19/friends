606222fbb064899a83733396484cf28ea442493a
shoaib019 <shoaibsabir019@gmail.com>
2021-10-29 18:58:23 +0500

    js implemented

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){ 
+    $(".pass").focusout(function(){
+        let val = $(".pass").val()
+        val.length >= 1?$("#info").hide(): $("#info").show()
+        val.length >= 6?$("#error").hide(): $("#error").show()
+    });
+    $(".pass2").focusout(function(){
+        let val = $(".pass2").val()
+        val.length >= 6?$("#error2").hide(): $("#error2").show()
+    })
+});
