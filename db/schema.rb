1ae6e0b5c862a457088e9e0d7e0b4a767588c2de
shoaib019 <shoaibsabir019@gmail.com>
2021-11-01 13:51:42 +0500

    change db

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20211028110709) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "friends", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
@@ -21,7 +24,7 @@ ActiveRecord::Schema.define(version: 20211028110709) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
-    t.index ["user_id"], name: "index_friends_on_user_id"
+    t.index ["user_id"], name: "index_friends_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,8 +36,8 @@ ActiveRecord::Schema.define(version: 20211028110709) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
